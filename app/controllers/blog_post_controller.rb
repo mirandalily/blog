a36b161dc07342dc099a47b9bc9dc982ef3afeaa
mirandalily <mirandaraymond@me.com>
2016-04-26 16:07:47 -0700

    basic set up

---
+++ app/controllers/blog_post_controller.rb
@@ -0,0 +1,78 @@
+class BlogPostController < ApplicationController
+
+  get '/posts' do
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      @posts = Post.all
+      erb :'posts/index'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/posts/new' do
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      erb :'posts/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/posts' do
+    if params[:content] == ""
+      redirect "/posts/new"
+    else
+      @user = User.find_by_id(session[:user_id])
+      @post = Post.create(:content => params[:content], :user_id => @user.id)
+      redirect "/posts/#{@post.id}"
+    end
+  end
+
+  get '/posts/:id' do
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      @post = Post.find_by_id(params[:id])
+      erb :'post/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    @user = User.find_by_id(session[:user_id])
+    if session[:user_id]
+      if @user == @post.user
+        erb :'/posts/edit_tweet'
+      else
+        redirect "/posts/#{@post.id}"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/posts/:id' do
+      if params[:content] == ""
+        redirect to "/posts/#{params[:id]}/edit"
+      else
+        @post = Post.find_by_id(params[:id])
+        @post.content = params[:content]
+        @post.save
+        redirect to "/posts/#{@post.id}"
+      end
+    end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @user = User.find_by_id(session[:user_id])
+    if @user == @post.user
+        @post.delete
+        redirect '/posts'
+    else
+      redirect "/posts/#{params[:id]}"
+    end
+  end
+
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,54 @@
+class UserController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/user/show'
+  end
+
+  get '/signup' do
+    if session[:user_id]
+      redirect '/posts'
+    else
+      erb :'user/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/posts'
+    end
+  end
+
+  get '/login' do
+    if session[:user_id]
+      redirect to '/posts'
+    else
+      erb :'user/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      erb :'user/login'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+end
